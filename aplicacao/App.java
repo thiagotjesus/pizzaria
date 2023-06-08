479b4a94ca661feee7bca7c7d82f9fb417cc3875
thiagotjesus <thiagotjesus@hotmail.com>
2023-06-07 22:04:29 -0300

    Update App.java

--- aplicacao/App.java
+++ aplicacao/App.java
@@ -1,5 +1,17 @@
 package aplicacao;
 
+import model.Pizza;
+
 public class App {
-    
+    public static void main(String[] args) {
+        Pizza p = new Pizza("Frango", 30, 'p');
+
+        System.out.println("Pedido criado!!!");
+        System.out.println(p.pedidoIfood());
+
+        Pizza p2 = new Pizza("camarão", 40, 'm');
+
+        System.out.println("Pedido criado!!!");
+        System.out.println(p2.pedidoIfood()); 
+    }
 }
